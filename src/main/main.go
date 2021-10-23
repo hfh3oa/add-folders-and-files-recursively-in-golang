cd34f210ed3a874ecc52003c4b638f6b2381b7fb
hfh3oa <ddr.dydy.0557@gmail.com>
2021-10-23 14:40:28 +0000

    ディレクトリ、ファイルの作成処理を実装

--- src/main/main.go
+++ src/main/main.go
@@ -1,8 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
 
 func main() {
 
-	fmt.Println("test")
+	src := "./input"
+	dest := "./output"
+
+	err := addEntry(src, dest, "")
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func addEntry(src, dest, path string) error {
+	srcPath := filepath.Join(src, path)
+	fs, err := os.Stat(srcPath)
+	if err != nil {
+		return err
+	}
+
+	if fs.IsDir() { // ディレクトリの場合
+		if err := addDir(src, dest, path); err != nil {
+			return err
+		}
+	} else { // ファイルの場合
+		if err := addFile(src, dest, path); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func addDir(src, dest, path string) error {
+	dp := filepath.Join(dest, path)
+	if err := os.Mkdir(dp, 0700); err != nil {
+		return err
+	}
+
+	sp := filepath.Join(src, path)
+	fi, err := os.ReadDir(sp)
+	if err != nil {
+		return err
+	}
+	for _, f := range fi {
+		err := addEntry(sp, dp, f.Name())
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func addFile(src, dest, path string) error {
+	d, err := ioutil.ReadFile(filepath.Join(src, path))
+	if err != nil {
+		return err
+	}
+
+	df, err := os.Create(filepath.Join(dest, path))
+	if err != nil {
+		return err
+	}
+	defer df.Close()
+
+	if _, err = df.Write(d); err != nil {
+		return err
+	}
+
+	return nil
 }
